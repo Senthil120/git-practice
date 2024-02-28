747d8cdf99b3d7d888a9de9f849fa780d6827895
Senthil120 <senthilsenthil7707@gmail.com>
2024-02-28 14:47:28 +0530

    message

---
+++ EvenOrOdd.java
@@ -0,0 +1,11 @@
+public class EvenOrOdd{
+public static void main(String args[]){
+  int num=12;
+  
+  if(num%2==0)
+   System.out.println("it is even");
+  else
+   System.out.println("it is Odd");
+
+
+}
